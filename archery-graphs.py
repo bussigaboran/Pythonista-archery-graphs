9dbb438af8b5aa513c218e8c45e671c1ed138816
Anders Bornäs <boran@mbp.mynet>
2019-12-29 23:00:16 +0100

    PEP8-ified the code

--- archery-graphs.py
+++ archery-graphs.py
@@ -13,27 +13,27 @@ all_dates = []
 
 title = indata.split('\n')[0]
 for line in indata.split('\n'):
-	if '-' in line:
-		# remove commas and split string on space
-		serie = line.rstrip().replace(',','').split(' ')
-		# move dates to new list
-		all_dates.append(str(serie.pop(0)))
-		# convert list elements to integers
-		all_series.append(list(map(int, serie)))
-		
+    if '-' in line:
+        # remove commas and split string on space
+        serie = line.rstrip().replace(',', '').split(' ')
+        # move dates to new list
+        all_dates.append(str(serie.pop(0)))
+        # convert list elements to integers
+        all_series.append(list(map(int, serie)))
+
 plt.boxplot((all_series), notch=False, sym="o")
 plt.title(title)
 plt.show()
 
-for plot_no, serie in enumerate(all_series, start = 1):
-	# Lines: no of graphs / 3 rounded up, Columns: 3, current plot number
-	plt.subplot(ceil(len(all_series) / 3.0), 3, plot_no)
-	plt.xlim(1,len(serie))
-	plt.ylim(0, 30)
-	date = all_dates[plot_no - 1]
-	title = "%s --> %d" % (date, mean(serie) * 10)
-	plt.title(title)
-	plt.scatter(range(1,len(serie) + 1), serie, marker='x')
+for plot_no, serie in enumerate(all_series, start=1):
+    # Lines: no of graphs / 3 rounded up, Columns: 3, current plot number
+    plt.subplot(ceil(len(all_series) / 3.0), 3, plot_no)
+    plt.xlim(1, len(serie))
+    plt.ylim(0, 30)
+    date = all_dates[plot_no - 1]
+    title = "%s --> %d" % (date, mean(serie) * 10)
+    plt.title(title)
+    plt.scatter(range(1, len(serie) + 1), serie, marker='x')
 
 plt.tight_layout()
 plt.show()
